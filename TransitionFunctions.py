ebe17c68633cbeef816a7093b7b445968cc1e6b5
ayarg <132581326+ayarg@users.noreply.github.com>
2023-05-08 04:09:23 +0200

    Update TransitionFunctions.py

--- TransitionFunctions.py
+++ TransitionFunctions.py
@@ -25,3 +25,26 @@ def transitionHeatON (n):
 
 P = transitionHeatON(n)
 print(P)
+
+
+
+
+
+
+def transitionHeatOFF (n):
+  PheatOFF = np.zeros((n,n))
+
+  for i in range(n):
+    for j in range(n):
+      if (j==i+1):
+        PheatOFF[i,j] = 0.1
+      else if (j==i):
+        PheatOFF[i,j] = 0.2
+      else if (j==i-1):
+        PheatOFF[i,j] = 0.7
+  PheatOFF[0,0]=0.9
+  PheatOFF[n-1, n-1] = 0.3
+  return PheatOFF 
+
+Q=transitionHeatOFF(n)
+print(Q)
